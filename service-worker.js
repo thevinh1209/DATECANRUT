0198f9fe187b68b7e63ca17b99be9a78d1f2368f
thevinh1209 <thevinh1209@gmail.com>
2026-05-01 11:27:19 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,15 @@
-const CACHE_NAME = "rut-date-v1";
-const urlsToCache = [
-  "./",
-  "./index.html"
-];
+const CACHE_NAME = "rut-date-v2";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match("./index.html"))
   );
 });
