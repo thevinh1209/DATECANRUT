73684a9007b20925042d21be101e719336a74a8d
thevinh1209 <thevinh1209@gmail.com>
2026-05-01 11:43:21 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,34 @@
-const CACHE_NAME = "rut-date-v3";
+const CACHE_NAME = "rut-date-final";
 
-self.addEventListener("install", e=>{
+const FILES = [
+  "./",
+  "./index.html",
+  "./rules.json",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e=>{
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => k !== CACHE_NAME && caches.delete(k))
+      )
+    )
+  );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    fetch(e.request).catch(()=>caches.match("./index.html"))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => res || fetch(event.request))
   );
 });
